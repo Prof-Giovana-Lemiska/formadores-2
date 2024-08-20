eaa11ddf00e0481e0b956bc8d66827642c9308d0
Giovana Lemiska <130910854+Prof-Giovana-Lemiska@users.noreply.github.com>
2024-08-20 06:58:13 -0300

    dd

--- script.js
+++ script.js
@@ -6,17 +6,45 @@ let textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1 - Você acha que o uso das plataformas está auxiliando no aprendizado?", 
-        alternativas: ["Sim, serve como um grande suporte", "Não, apenas uma obrigação e servindo apenas como certificação."]
-    }, 
-    {
-        enunciado: "Pergunta 2 - Os materiais educativos digitais podem ser personalizados para atender melhor às necessidades individuais de aprendizagem?", 
-        alternativas: ["A personalização dos recursos digitais pode proporcionar uma experiência de aprendizagem mais inclusiva e eficaz.", "A variedade de adaptações pode sobrecarregar e confundir os estudantes."]
+        enunciado: "Pergunta 1 - Você acha que o uso das plataformas está auxiliando no aprendizado?",
+        alternativas: [
+            {
+                texto: "Sim, serve como um grande suporte",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: "Não, apenas uma obrigação e servindo apenas como certificação.",
+                afirmacao: "Afirmação",
+            }
 
-    }, 
+        ]
+    },
     {
-        enunciado: "Pergunta 3 - Qual é, na sua opinião, o impacto das tecnologias e da disponibilidade de materiais educativos na educação?", 
-        alternativas: ["As tecnologias estão democratizando o acesso ao conhecimento e permitindo métodos de aprendizagem mais interativos e personalizados.", "A disponibilidade de materiais educativos online está facilitando a atualização constante de conteúdos e promovendo colaboração global entre estudantes e educadores."]
+        enunciado: "Pergunta 2 - Os materiais educativos digitais podem ser personalizados para atender melhor às necessidades individuais de aprendizagem?",
+        alternativas: [
+            {
+                texto: "A personalização dos recursos digitais pode proporcionar uma experiência de aprendizagem mais inclusiva e eficaz.",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: "A variedade de adaptações pode sobrecarregar e confundir os estudantes.",
+                afirmacao: "Afirmação",
+            }
+        ]
+
+    },
+    {
+        enunciado: "Pergunta 3 - Qual é, na sua opinião, o impacto das tecnologias e da disponibilidade de materiais educativos na educação?",
+        alternativas: [
+            {
+                texto: "As tecnologias estão democratizando o acesso ao conhecimento e permitindo métodos de aprendizagem mais interativos e personalizados.",
+                afirmacao: "Afirmação",
+            },
+            {
+                texto: "A disponibilidade de materiais educativos online está facilitando a atualização constante de conteúdos e promovendo colaboração global entre estudantes e educadores.",
+                afirmacao: "Afirmação",
+            }
+        ]
 
     }
 ];
@@ -31,10 +59,14 @@ function mostraPerguntas() {
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for(const alternativas of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativas of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativas;
+        botaoAlternativas.textContent = alternativas.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostraPerguntas();
+        })
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
